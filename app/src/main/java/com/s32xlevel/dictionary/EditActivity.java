06828a2f359dac1fa888b6d60d491d4cfadeb880
Artur Ilkaev <xaysdiablo@gmail.com>
2019-04-01 16:35:31 +0500

    prepare to realize Ya.TR API

--- app/src/main/java/com/s32xlevel/dictionary/EditActivity.java
+++ app/src/main/java/com/s32xlevel/dictionary/EditActivity.java
@@ -5,20 +5,37 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class EditActivity extends AppCompatActivity {
 
     public static final String EXTRA_WORD_ID = "wordId";
-
     private SQLiteDatabase db;
+//    private static final String YANDEX_URL = "https://translate.yandex.net/api/v1.5/tr/translate?key=trnsl.1.1.20190323T155359Z.d61b1e6ec9c257cd.2e2e095ce68ff4fa10bafe21579e4fb1af687999";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +43,7 @@ public class EditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        // Settings dependent on EXTRA_WORD_ID
+        // Settings dependent on EXTRA_WORD_ID //
         if (getIntent().getIntExtra(EditActivity.EXTRA_WORD_ID, -1) != -1) {
             toolbar.setTitle(getString(R.string.edit_word_title));
             fillEditViews();
@@ -35,6 +52,8 @@ public class EditActivity extends AppCompatActivity {
         }
         setSupportActionBar(toolbar);
         fillCorrectTextForButton();
+        //                                    //
+//        onKeyPressTextToTranslate();
     }
 
     @Override
@@ -112,4 +131,98 @@ public class EditActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+/*    private void onKeyPressTextToTranslate() {
+        EditText ruWord = findViewById(R.id.ru_word_edit);
+        ruWord.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    fillListHints(s.toString());
+                } catch (Exception e) {
+                    Toast.makeText(EditActivity.this, "Problem with translate API", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+    }
+
+    private void fillListHints(String text) throws Exception {
+        ListView listHints = findViewById(R.id.list_hints);
+        final EditText enWords = findViewById(R.id.en_word_edit);
+        final String[] hints = new AsyncRequest().execute(text, "ru-en").get();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, hints);
+        listHints.setAdapter(adapter);
+
+        listHints.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                enWords.setText(hints[position]);
+            }
+        });
+    }
+
+    class AsyncRequest extends AsyncTask<String, Void, String[]> {
+        // param[0] - text to translate, params[1] - languages (for example ru-en)
+        @Override
+        protected String[] doInBackground(String... params) {
+            try {
+                URL url = new URL(EditActivity.YANDEX_URL);
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+                dataOutputStream.writeBytes("text=" + URLEncoder.encode(params[0], "UTF-8") + "&lang=" + params[1]);
+
+                InputStream response = connection.getInputStream();
+                String json = new java.util.Scanner(response).nextLine();
+
+                YandexTranslateResponse ytr = new Gson().fromJson(json, YandexTranslateResponse.class);
+
+                dataOutputStream.close();
+                response.close();
+
+                return ytr.getText();
+            } catch (IOException e) {
+                Toast.makeText(EditActivity.this, "Problem with translate API", Toast.LENGTH_LONG).show();
+            }
+            return null;
+        }
+    }
+
+    class YandexTranslateResponse {
+        private int code;
+        private String lang;
+        private String[] text;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getLang() {
+            return lang;
+        }
+
+        public String[] getText() {
+            return text;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public void setLang(String lang) {
+            this.lang = lang;
+        }
+
+        public void setText(String[] text) {
+            this.text = text;
+        }
+    }*/
 }
